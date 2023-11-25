f22763b5d345d8734fe61e6cb2a98f3a58db3273
Renaud <renaud.yoganathan@efrei.net>
2023-11-25 15:14:56 +0100

    Init

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import os
-from function import *
+from function import list_of_files, minuscule
 
-# on importe les fichiers
 directory = "./speeches"
-copy_folder = "/cleaned"
+copy_folder = "./cleaned"
 
-speeches_names = list_of_files(directory, "txt")
+speechesNominationList = list_of_files(directory, "txt")
+copy_truc = list_of_files(copy_folder, "txt")
 
 list_names = []
 names = []
 
-for i in range(len(speeches_names)):
-    nom = str(speeches_names[i][11:-4])   # on slice la liste pour n'avoir que le nom du président et le numéro
-    list_names.append(nom)
+for i in range(len(speechesNominationList)):
+    speeches_without_numbers = str(speechesNominationList[i][11:-4])
+    list_names.append(speeches_without_numbers)
 
-# On a encore des chiffres, il faut donc les supprimer
 for i in list_names:
+
     if i[-1].isdigit():
         name = i[:-1]
-    # Si le dernier caractère est un chiffre alors on va slice la liste de façon à reculer d'un pas
     else:
         name = i
-    # S'il n'y a pas de chiffre alors on ne fait rien
+
     names.append(name)
 
-#on supprime les doublons
-president_names = set(names)             
+president_names = set(names)
 president_names = list(president_names)
 
-print("Voici les noms des présidents ayant fait un discour : ")
+print("Voici les noms des présidents ayant fait des discours : ")
+
 for i in range(len(president_names)):
+
     print(president_names[i], end=", ")
-#on copie les discours dans des autres fichiers afin de les traiter
 
+for i in speechesNominationList:
 
-cleaning_chirac1()
+    for y in copy_truc:
+
+        minuscule(speechesNominationList[i], copy_truc[y])
